8bd765b98d80fa79915b0bc15340da5da16e2979
Christopher Thai <christopherthai.nyli@gmail.com>
2024-05-03 15:02:14 -0400

    fix: Fix issue with navbar color will change in admin page

--- client/src/components/Admin/ManageRestaurants.jsx
+++ client/src/components/Admin/ManageRestaurants.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
 import {
   //   AppBar,
   //   Toolbar,
@@ -10,7 +9,6 @@ import {
   ListItemText,
   makeStyles,
   Grid,
-  Paper,
 } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
@@ -24,7 +22,7 @@ const useStyles = makeStyles((theme) => ({
   drawerPaper: {
     width: 200,
     height: "100vh",
-    marginTop: "64px",
+    marginTop: "64px", // Add this line
   },
 }));
 
@@ -38,63 +36,33 @@ function ManageRestaurant() {
     },
   ];
 
-  const [restaurants, setRestaurants] = useState([]);
-
-  //   useEffect(() => {
-  //     fetch("/api/restaurants") // replace with your actual API endpoint
-  //       .then((response) => response.json())
-  //       .then((data) => setRestaurants(data));
-  //   }, []);
-
   return (
-    <>
-      <Grid container direction="column">
-        <Grid item>
-          {/* <AppBar position="static" className={classes.appBar}>
+    <Grid container direction="column">
+      <Grid item>
+        {/* <AppBar position="static" className={classes.appBar}>
           <Toolbar>
             <Typography variant="h6">Admin Dashboard</Typography>
           </Toolbar>
         </AppBar> */}
-        </Grid>
-        <Grid item>
-          <Drawer
-            className={classes.drawer}
-            variant="permanent"
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-          >
-            <List>
-              {drawerItems.map((item, index) => (
-                <ListItem button key={index} component={Link} to={item.path}>
-                  <ListItemText primary={item.name} />
-                </ListItem>
-              ))}
-            </List>
-          </Drawer>
-        </Grid>
       </Grid>
-      <Grid
-        container
-        direction="column"
-        alignItems="center"
-        justify="center"
-        style={{ minHeight: "100vh" }}
-      >
-        <Grid item>
-          <Paper elevation={3}>
-            <List>
-              {restaurants.map((restaurant, index) => (
-                <ListItem button key={index}>
-                  <ListItemText primary={restaurant.name} />{" "}
-                  {/* replace 'name' with the actual property name */}
-                </ListItem>
-              ))}
-            </List>
-          </Paper>
-        </Grid>
+      <Grid item>
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <List>
+            {drawerItems.map((item, index) => (
+              <ListItem button key={index} component={Link} to={item.path}>
+                <ListItemText primary={item.name} />
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
       </Grid>
-    </>
+    </Grid>
   );
 }
 
